71fd1f3b65e6054b9fe8197b096254b7f68307d7
Mohan <mohan.kst.hp@gmail.com>
2017-05-13 03:46:17 +0600

    Moved email sending function to separate file, added feature to get replay links from sheet

---
+++ mail_login.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+#importing local files
+from sheet_control import update_report_cell
+
+
+
+
+def login_gmail(driver, email, password, remail, cell_value):
+	mail_problem = False
+	#Selenium
+	#chrome_options = Options()
+	#chrome_options.add_argument('--dns-prefetch-disable')
+	driver.get("https://accounts.google.com/Login")
+	wait = WebDriverWait(driver, 10)
+	gmail_user_id_field = wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="identifierId"]')))
+	gmail_user_id_field.click()
+	gmail_user_id_field.send_keys(email)
+	gmail_user_id_field.send_keys(Keys.RETURN)
+	gmail_password_field = wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="password"]/div[1]/div/div[1]/input')))
+	gmail_password_field.send_keys(password)
+	gmail_password_field.send_keys(Keys.RETURN)
+	try:
+		remail_field = wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="view_container"]/form/div[2]/div/div/div/ul/li[1]/div/div[2]')))
+		remail_field.click()
+		remail_field_imput = wait.until(EC.element_to_be_clickable((By.ID, 'knowledge-preregistered-email-response')))
+		remail_field_imput.send_keys(remail)
+		remail_field_imput.send_keys(Keys.RETURN)
+	except:
+		pass
+	# Print reporting date to google sheet after log in
+	update_report_cell(cell_value)
+
+	try:
+		wait.until(EC.element_to_be_clickable((By.XPATH, '//*[text()[contains(.,"Account disabled")]]')))
+		message = "Problem (Account disabled)"
+		err_report(cell_value, message)
+		driver.quit()
+		mail_problem = True
+	except:
+		pass
+	return mail_problem
