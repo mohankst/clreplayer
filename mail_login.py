9b753a6116f288682dc6b0d81fec3f5bf8de39c8
Mohan <mohan.kst.hp@gmail.com>
2017-05-16 20:22:06 +0600

    imported random

--- mail_login.py
+++ mail_login.py
@@ -1,4 +1,5 @@
-from selenium import webdriver, random
+import random
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
